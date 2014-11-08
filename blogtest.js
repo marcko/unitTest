5b316a5db0d7a2e3d7a1fcd370279ac946b9b3cf
marcko <marco.itnl@gmail.com>
2014-11-08 00:24:12 -0600

    se agrega after hook funcionando

--- blogtest.js
+++ blogtest.js
@@ -1,12 +1,23 @@
+var fs = require("fs");
 var should = require('should');
 var assert = require("assert");
 var blogfs = require("../api/lib/blogfs");
 var blogger = new blogfs();
-
-
+var created="";
+  
 describe('realizara las pruebas de la clase blogfs', function () {
-
-  it('debe de crear un archivo .json con contenido formateado', function (done) {
+  describe('debe borra archivos creados', function () {
+    afterEach(function (done) {
+        var path = "/home/marcko/practicasjs/fs/blogs/";
+       fs.unlink(path+created+".json", function(err){   
+          if(err) return console.log(err);
+          return console.info("deleted");     
+        });
+       
+       done();
+      });
+      
+       it('debe de crear un archivo .json con contenido formateado', function (done) {
 
     var input={
       title:"hola",
@@ -19,7 +30,7 @@ describe('realizara las pruebas de la clase blogfs', function () {
       blogger.should.be.an.instanceof(blogfs);
       blogger.should.be.an.instanceof(Object);
       blog.should.be.type("string");
-
+      created = blog;
       done();
 
 
@@ -27,5 +38,7 @@ describe('realizara las pruebas de la clase blogfs', function () {
 
     
   });
+  });
+ 
   
 });
